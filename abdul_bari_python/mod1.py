a05912e98c133aa7052bbfd8dca3499f5d52aac9
aaditya2291 <ayyala.aditya.2291@gmail.com>
2024-12-28 09:14:00 +0530

    exception_handling

---
+++ abdul_bari_python/mod1.py
@@ -0,0 +1,19 @@
+Total_Amount = 20000
+
+
+def add(x, y):
+    """addition"""
+    return x + y
+
+
+def sub(x, y):
+    """subtract"""
+    return x - y
+
+
+
+if __name__ == '__main__':
+    print(__name__)
+    print("from mod1",add(50,20))
+    print("from mod1",sub(50,20))
+    print("from mod1",Total_Amount)
---
+++ abdul_bari_python/mod2.py
@@ -0,0 +1,8 @@
+from mod1 import *
+
+
+print(Total_Amount)
+
+print(add(5,20))
+
+print(sub(20,5))
